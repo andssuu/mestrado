ce241855afff7119289b2b32db631060ee5d2be3
Anderson <anderson.silvasantos@ufrpe.br>
2020-04-22 12:01:35 -0300

    add filtro no mapa de calor para o intervalos de horas

--- main/exp_1/maps/gps_transform.py
+++ main/exp_1/maps/gps_transform.py
@@ -1,15 +1,13 @@
 import json
 
-cows = {'1': 'branquinha', '2':'mimosa', '3':'pretinha', '4': 'chuviscada'}
 if __name__ == "__main__":
-    for n in cows.keys():
-        cow = []
-        file = open('mestrado/data/dados-jua-ams/experimento-01/unified-gps-0{}.txt'.format(n), 'r')
-        for line in file.readlines():
-            ts, lat, lng= line.strip().split(';')
-            obj = {}
-            obj['ts'], obj['lat'], obj['lng'] = ts, lat, lng
-            cow.append(obj)
-        with open('mestrado/data/exp_1/cow_{}.json'.format(n), 'w') as f: f.write('cow_{} = '.format(n))
-        with open('mestrado/data/exp_1/cow_{}.json'.format(n), 'a') as f: json.dump(cow, f, indent=2)
-        f.close()
+    cow = []
+    file = open('mestrado/data/exp_1/20190807124800-gps.txt', 'r')
+    for line in file.readlines():
+        ts, lat, lng = line.strip().split(';')
+        obj = {}
+        obj['ts'], obj['lat'], obj['lng'] = ts, lat, lng
+        cow.append(obj)
+    with open('mestrado/data/exp_1/chuviscada.json', 'w') as f: f.write('chuviscada = ')
+    with open('mestrado/data/exp_1/chuviscada.json', 'a') as f: json.dump(cow, f, indent=2)
+    f.close()
