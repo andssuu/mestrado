4134a87d57b241a21b252ee5a495422d8f122650
Anderson <anderson.silvasantos@ufrpe.br>
2020-06-04 15:46:20 -0300

    alterando intervalo de tempo para 1 min

--- main/exp_1/acc_boxplot_minute_std.py
+++ main/exp_1/acc_boxplot_minute_std.py
@@ -24,17 +24,17 @@ if __name__ == "__main__":
         time_std = range(0, 600, 100)
         values_x, values_y, values_z, values_resultant = [], [], [], []
         _data = []
-        for j in time_std:
-            _x = np.array([ax[i] for i in index[j:j+100]])
-            _y = np.array([ay[i] for i in index[j:j+100]])
-            _z = np.array([az[i] for i in index[j:j+100]])
-            _x_mean = np.mean(_x)
-            _y_mean = np.mean(_y)
-            _z_mean = np.mean(_z)
-            _x_std = np.std(_x)
-            _y_std = np.std(_y)
-            _z_std = np.std(_z)
-            labels = ['Acc X', 'Acc Y', 'Acc Z']
+        # for j in time_std:
+        #     _x = np.array([ax[i] for i in index[j:j+100]])
+        #     _y = np.array([ay[i] for i in index[j:j+100]])
+        #     _z = np.array([az[i] for i in index[j:j+100]])
+        #     _x_mean = np.mean(_x)
+        #     _y_mean = np.mean(_y)
+        #     _z_mean = np.mean(_z)
+        #     _x_std = np.std(_x)
+        #     _y_std = np.std(_y)
+        #     _z_std = np.std(_z)
+        #     labels = ['Acc X', 'Acc Y', 'Acc Z']
             # x_pos = np.arange(len(labels))
             # CTEs = [_x_mean, _y_mean, _z_mean]
             # error = [_x_std, _y_std, _z_std]
@@ -48,10 +48,22 @@ if __name__ == "__main__":
             # # Save the figure and show
             # plt.tight_layout()
             # plt.show()
-            fig, graph = plt.subplots()
-            labels = ['Acc X', 'Acc Y', 'Acc Z']
-            graph.set_xticklabels(labels)
-            graph.set_title('08/08/19 {}m {}s {}s'.format(n, int(j/10), int(j/10)+10))
-            bp = graph.boxplot([_x, _y, _z])
-            plt.ylabel('Acc (g)')
-            plt.show()
+        #     fig, graph = plt.subplots()
+        #     labels = ['Acc X', 'Acc Y', 'Acc Z']
+        #     graph.set_xticklabels(labels)
+        #     graph.set_title('08/08/19 {}m {}s {}s'.format(n, int(j/10), int(j/10)+10))
+        #     bp = graph.boxplot([_x, _y, _z])
+        #     plt.ylabel('Acc (g)')
+        #     plt.show()
+        _x = np.array([ax[i] for i in index])
+        _y = np.array([ay[i] for i in index])
+        _z = np.array([az[i] for i in index])
+        fig, graph = plt.subplots()
+        labels = ['Acc X', 'Acc Y', 'Acc Z']
+        graph.set_xticklabels(labels)
+        graph.set_title('08/08/19 {}h {}m:{}m'.format(hour, n, n+1))
+        bp = graph.boxplot([_x, _y, _z])
+        plt.ylabel('Acc (g)')
+        #plt.savefig('mestrado/figures/exp1/accelerometer/{}h/boxplot/exp1_acc_bp_{}-{}.png'.format(hour, n, n+1))
+        #plt.close()
+        plt.show()
