2be9176b976329478abc2da33bab4e64f040f230
Anderson <anderson.silvasantos@ufrpe.br>
2020-08-13 15:03:04 -0300

    refatoracao para remover inclusao de bibliotecas e variaveis desnecessarias

--- tics_4/node_mcu/acc_tb/acc_tb.ino
+++ tics_4/node_mcu/acc_tb/acc_tb.ino
@@ -2,50 +2,27 @@
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <TimeAlarms.h>
-
-/* ssid e password do AP ao qual se deseja conectar */
-const char* ssid = "...";
-const char* password = "!123?QWE...";
-/* endereco do servidor onde está instalado o ThingsBoard
-// const char* mqtt_server = "192.168.X.XXX";
-/* ou para o ThingsBoard live demo */
-//const char* mqtt_server = "demo.thingsboard.io";
-const char* mqtt_server = "10.0.0.104";
-/* credenciais para o device que se deseja conectar */
-//const char* device_token = "cfttoHJ3kIFalrii4EMK"; // THINGSBOARD DEMO
-const char* device_token = "Cy6XWLVDOVdFgjcexVSR"; // THINGSBOARD LOCAL
-/* instancia o cliente Wi-Fi */
-WiFiClient wifi_client;
-/* instancia o cliente MQTT */
-PubSubClient mqtt_client(wifi_client);
-double _time;
-double  _time_plus;
-double  last_time;
-//unsigned int _time2;
-double  _time_mili;
-
-// Define NTP Client to get time
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", 0); //set uct-0
-
-// --------------- ACC BEGIN ---------------
 #include "I2Cdev.h"
-#include <FS.h>
 #include "MPU6050_6Axis_MotionApps20.h"
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
+
+const char* ssid = "...";
+const char* password = "!123?QWE...";
+const char* mqtt_server = "demo.thingsboard.io";
+//const char* mqtt_server = "10.0.0.101";
+const char* device_token = "XM5DQeXupU6OzvF06sDP"; // THINGSBOARD DEMO
+//const char* device_token = "7SWvR7FGz8Rf77jG5l52"; // THINGSBOARD LOCAL
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+double _time;
+double  _time_plus;
+double  last_time;
+double  _time_mili;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0); //set uct-0
 MPU6050 mpu;
-
-// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
-// components with gravity removed and adjusted for the world frame of
-// reference (yaw is relative to initial orientation, since no magnetometer
-// is present in this case). Could be quite handy in some cases.
-#define OUTPUT_READABLE_WORLDACCEL
-
-#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -54,8 +31,6 @@ uint8_t devStatus;      // return status after each device operation (0 = succes
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
-VectorInt16 fifoBufferAcc[15];
-
 // orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
@@ -72,21 +47,17 @@ volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin h
 void dmpDataReady(){
   mpuInterrupt = true;
 }
-// --------------- ACC END --------------- 
 
 void conectar_wifi() {
   delay(10);
   Serial.println();
   Serial.print("conectando a ");
   Serial.println(ssid);
-  /* tenta iniciar a conexao com a rede Wi-Fi */
   WiFi.begin(ssid, password);
-  /* verifica o status e aguarda a conexao */
   while(WiFi.status()!=WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
-  /* imprime na serial IP da conexao Wi-Fi */
   Serial.println("");
   Serial.println("conectado ao AP");
   Serial.println("endereco IP: ");
@@ -94,15 +65,11 @@ void conectar_wifi() {
 }
 
 void reconectar(){
-  /* loop ate que esteja reconectado com o broker MQTT */
   while(!mqtt_client.connected()){
-    /* verifica a conexao Wi-Fi */
     if (WiFi.status() != WL_CONNECTED) {
-      /* reconecta com o Wi-Fi */
       conectar_wifi();
     }
     Serial.print("tentando conexao com o ThingsBoard via MQTT...");
-    /* tentativa de conexao */
     if (mqtt_client.connect("wemos", device_token, NULL)) {
       Serial.println("conectado");
     } 
@@ -110,23 +77,15 @@ void reconectar(){
       Serial.print("falhou, rc=");
       Serial.print(mqtt_client.state());
       Serial.println(" tentando novamente em 5s");
-      /* tenta novamente daqui a 5s */
       delay(5000);
     }
   }
 }
-// ================================================================
-// ===                      INITIAL SETUP                       ===
-// ================================================================
 
 void setup(){
-  /* configura a interface serial */
   Serial.begin(115200);
-  /* conectar a rede Wi-Fi */
   conectar_wifi();
-  /* conectar ao broker MQTT */
   mqtt_client.setServer(mqtt_server, 1883);
-  // join I2C bus (I2Cdev library doesn't do this automatically)
   #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
       Wire.begin(D5, D6);
       Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
@@ -134,12 +93,9 @@ void setup(){
       Fastwire::setup(400, true);
   #endif
   while (!Serial);
-  Serial.println(F("Initializing I2C devices..."));
   mpu.initialize();
-  pinMode(INTERRUPT_PIN, INPUT);
+  //pinMode(INTERRUPT_PIN, INPUT);
   // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
   // supply your own gyro offsets here, scaled for min sensitivity
@@ -156,7 +112,7 @@ void setup(){
       // turn on the DMP, now that it's ready
       Serial.println(F("Enabling DMP..."));
       mpu.setDMPEnabled(true);
-      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      Serial.println(F("DMP ready!"));
       dmpReady = true;
       // get expected DMP packet size for later comparison
       packetSize = mpu.dmpGetFIFOPacketSize();
@@ -182,13 +138,8 @@ void setup(){
   last_time = millis();
 }
 
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
-
 void loop(){
-  // if(!dmpReady) return;
-  // read a packet from FIFO
+  if(!dmpReady) return;
   if(mpu.dmpGetCurrentFIFOPacket(fifoBuffer)){ // Get the Latest packet 
     //Serial.println(_time);
     //Serial.println(_time*1000);
@@ -207,21 +158,15 @@ void loop(){
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
     mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-    /* cria o payload da mensagem MQTT no formato JSON */
-    //Serial.println(_time1*1000);
+    // Serial.println(_time1*1000);
     String payload ="{\"ts\":"+String(_time, 0)+",\"values\":{\"x\":"+String(aaWorld.x)+",\"y\":"+String(aaWorld.y)+",\"z\":"+String(aaWorld.z)+"}}";
     Serial.println(payload);
-    //{"ts":1451649600512, "values":{"key1":"value1", "key2":"value2"}}
-    /* converte os dados em formato JSON de String para char[] */
     char dados_json[100];
     payload.toCharArray(dados_json, 100);
-    /* envia o pacote MQTT com os dados dos sensores */
     mqtt_client.publish( "v1/devices/me/telemetry", dados_json);
   }
-  /* verifica status da conexao */
   if(!mqtt_client.connected()){
     reconectar();
   }
-  /* executa a funcao de loop do cliente MQTT */
   mqtt_client.loop();
 }
